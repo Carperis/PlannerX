acea25f1ebce5ffef6a2bcfb47b0dfe0bed7f557
pengqiu <qiupeng@bu.edu>
2023-08-13 19:18:23 -0400

    update feedback form

---
+++ static/js/my_js.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+
+    $("#report_form").hide();
+    $("#survey_form").hide();
+
+  // When the button is focused, show the content div
+  $("#feedback_btn").on("click", function () {
+    if ($("#feedback_form").hasClass("float_content_active")) {
+      $("#feedback_form").removeClass("float_content_active");
+    } else {
+      $("#feedback_form").addClass("float_content_active");
+    }
+  });
+
+  // When the button loses focus, hide the content div
+  $("body").on("mousedown", function () {
+    if (!$("#feedback_form").is(":hover") && !$("#feedback_btn").is(":hover")){
+      $("#feedback_form").removeClass("float_content_active");
+    }
+  });
+
+  $("#survey_btn").on("click", function () {
+    $("#survey_form").show();
+    $("#report_form").hide();
+  });
+
+  $("#report_btn").on("click", function () {
+    $("#report_form").show();
+    $("#survey_form").hide();
+  });
+
+});
